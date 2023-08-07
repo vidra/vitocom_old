c0468a592603a87a61e51b2a38b09e8ea08832c0
Tatiana_K <ottertut@gmail.com>
2023-08-07 16:39:22 +0300

    bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ extend: {
       gridTemplateColumns: {
         '5': '10% 20% 40% 20% 10%',
       },
+      backgroundImage: {
+         'hero-pattern': "linear-gradient(to right bottom, rgba('#ffffff',0.5), rgba('#9eb9ce',0.5)), url('img_foot.png')",
+      },
     },
 
   },
